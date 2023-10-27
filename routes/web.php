d118b150d3b0374175bdd442143f41aa71a39c5c
nevvrmbk <99172207+nevvrmbk@users.noreply.github.com>
2023-10-27 11:11:52 +0300

    Added deploy.sh

--- routes/web.php
+++ routes/web.php
@@ -19,20 +19,20 @@ use App\Http\Controllers\ItemController;
 Route::view("/", "index");
 Route::get('/items', [ItemController::class, "index"]);
 Route::get("/items/create", [ItemController::class, "create"]);
-Route::post("/items", [ItemController::class, "store"]);
+Route::post("/items", [ItemController::class, "store"])->middleware("auth");
 Route::get("/items/edit/{item}", [ItemController::class, "edit"]);
-Route::put("/items/{item}", [ItemController::class, "update"]);
-Route::delete("/items/{item}", [ItemController::class, "destroy"]);
+Route::put("/items/{item}", [ItemController::class, "update"])->middleware("auth");
+Route::delete("/items/{item}", [ItemController::class, "destroy"])->middleware("auth");
 
 Route::get("/search", function () {
     $items = Item::search(request("q"))->paginate();
     return view("item.index", ["items"=> $items]);
 });
 
-Route::get("/login", [AuthController::class, "login"]);
-Route::post("/login", [AuthController::class, "signin"]);
-Route::get("/signup", [AuthController::class, "signup"]);
-Route::post("/signup", [AuthController::class, "register"]);
+Route::get("/login", [AuthController::class, "login"])->middleware("guest");
+Route::post("/login", [AuthController::class, "signin"])->middleware("guest");
+Route::get("/signup", [AuthController::class, "signup"])->middleware("guest");
+Route::post("/signup", [AuthController::class, "register"])->middleware("guest");
 
 Route::get("/categories");
 
